4561ba738189148d6788a6831c9745f27a9997f5
labnova <progettofeb@gmail.com>
2014-07-22 12:47:58 +0200

    progetto finale iOS

---
+++ Data.h
@@ -0,0 +1,13 @@
+//
+//  Data.h
+//  BloccoNote
+//
+//  Created by Innocenzo Tremamondo on 18/07/14.
+//  Copyright (c) 2014 LabNova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Data : NSObject
+
+@end
---
+++ Data.m
@@ -0,0 +1,13 @@
+//
+//  Data.m
+//  BloccoNote
+//
+//  Created by Innocenzo Tremamondo on 18/07/14.
+//  Copyright (c) 2014 LabNova. All rights reserved.
+//
+
+#import "Data.h"
+
+@implementation Data
+
+@end
